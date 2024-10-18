43b9927d58eba9a967eff6665554c9fa5869cda7
Sotatek-ChienNguyen2 <chien.nguyen2@sotatek.com>
2024-10-17 19:49:20 -0700

    update mutex lock

--- 00Day1/src/ringbuf.c
+++ 00Day1/src/ringbuf.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "ringbuf.h"
 #include <stdint.h>
+#include <pthread.h>
+
+#include "ringbuf.h"
+
+
+pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
 
 RingBuffer_t r;
 
@@ -14,7 +19,6 @@ void ringbuffer_init(int size)
     r.head = 0;
     r.tail = 0;
     r.size = 0;
-
 }
 
 int ringbuffer_add(int value)
@@ -22,9 +26,11 @@ int ringbuffer_add(int value)
     if(r.size == r.max_size){
         return -1;
     }
+    pthread_mutex_lock(&lock);
     r.buffer[r.tail] = value;
     r.tail = (r.tail +1) % r.max_size; // nếu quá max sẽ quay lại 0;
     r.size++;
+    pthread_mutex_unlock(&lock);
     return 0;
 }
 
@@ -34,8 +40,10 @@ int ringbuffer_remove(int *value)
         return -1;
     }
     *value = r.buffer[r.head];
+    pthread_mutex_lock(&lock);
     r.head = (r.head + 1) % (r.max_size); //nếu vượt quá sẽ quay lại 0
     r.size --;
+    pthread_mutex_unlock(&lock);
     
     return 0;
 
@@ -45,43 +53,27 @@ int ringbuffer_remove(int *value)
 int ringbuffer_is_full()
 {
    return (r.size == r.max_size) ? 1 : 0;
-   // return ((r.tail + 1) % r.max_size == r.head) ? 1 : 0;
+   
 }
 
 int ringbuffer_is_empty()
 {
     return (r.size == 0) ? 1 : 0;
-    //return (head == tail) ? 1 : 0;
-
 
 }
 
 int ringbuffer_size()
 {
     return r.size;
-
-
 }
 
 
-
-
-int ringbuffer_get_head() {
-    return r.head;
-}
-
-int ringbuffer_get_tail() {
-    return r.tail;
-}
-
-
-// Hàm in trạng thái của buffer
 void ringbuffer_print() {
     printf("Trạng thái của buffer: ");
     for (int i = 0; i < r.max_size; i++) {
-        int index = (r.head + i) % r.max_size;  // Tính toán chỉ số
+        int index = (r.head + i) % r.max_size;  
         if (i < r.size) {
-            printf("%d ", r.buffer[index]); // In giá trị nếu có
+            printf("%d ", r.buffer[index]); 
         } else {
             printf("N/A "); // In N/A cho ô trống
         }
